b8ac2809a3cd3cc394103ccad58dd05619cbc93f
zhoujinhai <932853432@qq.com>
2024-11-28 16:14:38 +0800

    support numpy to showRot

--- tools/visual_utils/testRot.py
+++ tools/visual_utils/testRot.py
@@ -61,9 +61,43 @@ def boxes_to_corners_3d(boxes3d):
     return corners3d.numpy() if is_numpy else corners3d
 
 
+def rotate_points_along_z_numpy(points, angle):
+    cosa = np.cos(angle)
+    sina = np.sin(angle)
+    zeros = np.zeros_like(angle)
+    ones = np.ones_like(angle)
+    
+    # 构建旋转矩阵
+    rot_matrix = np.stack((
+        cosa, sina, zeros,
+        -sina, cosa, zeros,
+        zeros, zeros, ones
+    ), axis=1).reshape(-1, 3, 3)
+    
+    # 执行旋转
+    points_rot = np.einsum('bij, bjk -> bik', points[:, :, :3], rot_matrix)
+    
+    # 合并旋转后的点和其他维度
+    points_rot = np.concatenate((points_rot, points[:, :, 3:]), axis=-1)
+    return points_rot
+
+def boxes_to_corners_3d_numpy(boxes3d):
+    template = np.array((
+        [1, 1, -1], [1, -1, -1], [-1, -1, -1], [-1, 1, -1],
+        [1, 1, 1], [1, -1, 1], [-1, -1, 1], [-1, 1, 1],
+    )) / 2
+
+    corners3d = boxes3d[:, None, 3:6] * template[None, :, :]
+    corners3d = rotate_points_along_z_numpy(corners3d.reshape(-1, 8, 3), boxes3d[:, 6]).reshape(-1, 8, 3)
+    corners3d += boxes3d[:, None, :3]
+
+    return corners3d
+
+ 
 if __name__ == "__main__":
     box = np.asarray([2.329600500773367, 8.582458537044301, 0.7314550633201173, 0.9060115816526095, 1.0885560636920202, 3.4163465986710446, -1.4922565104551522])
     print(box.shape)
     box = box.reshape(1, -1)
-    print(boxes_to_corners_3d((box)))
+    # print(boxes_to_corners_3d((box)))
+    print(boxes_to_corners_3d_numpy(box))
 
